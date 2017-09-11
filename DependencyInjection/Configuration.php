c4a5c863ab8cbf6da5ae0200e6f1d1dcf29af668
Evans Mwendwa <evansmwendwa@users.noreply.github.com>
2017-09-11 19:36:55 +0300

    register configurable nodes

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -4,6 +4,7 @@ namespace Evans\FilepickerBundle\DependencyInjection;
 
 use Symfony\Component\Config\Definition\Builder\TreeBuilder;
 use Symfony\Component\Config\Definition\ConfigurationInterface;
+use Symfony\Component\Config\Definition\Builder\ArrayNodeDefinition;
 
 /**
  * This is the class that validates and merges configuration from your app/config files.
@@ -23,7 +24,42 @@ class Configuration implements ConfigurationInterface
         // Here you should define the parameters that are allowed to
         // configure your bundle. See the documentation linked above for
         // more information on that topic.
+        $this->registerOptions($rootNode);
 
         return $treeBuilder;
     }
+
+    /**
+    * Add configuration Picker
+    *
+    * @param ArrayNodeDefinition $rootNode
+    */
+   private function registerOptions(ArrayNodeDefinition $rootNode)
+   {
+       $rootNode
+           ->children()
+               ->scalarNode('uploads_destination')
+                    ->info('Provide uploads destination path here. Relative to web directory')
+                    ->defaultValue('/uploads/chooser')
+               ->end()
+
+               ->booleanNode('allow_uploads')
+                    ->info('Do you want users to be able to use the uploads feature')
+                    ->defaultValue(true)
+               ->end()
+
+               ->booleanNode('group_files')
+                    ->info('Do you want files grouped by months')
+                    ->defaultValue(false)
+               ->end()
+
+               ->arrayNode('allowed_mimes')
+                    ->prototype('scalar')
+                    ->end()
+                    ->defaultValue(['image/jpeg','image/png','image/gif'])
+               ->end()
+
+           ->end()
+       ;
+   }
 }
